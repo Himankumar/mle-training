9e63fe80fce5fd24e8276fd4fe0d4970f20a760d
Himanshu Kumar <himanshu.kumar2687@tigeranalytics.com>
2023-03-16 02:14:02 +0530

    Run python code and add unnecessary folder to gitignore

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -21,11 +21,13 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 import pandas as pd
 
+fetch_housing_data()
+
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-housing = load_housing_data
+housing = load_housing_data()
 
 from sklearn.model_selection import train_test_split
 
